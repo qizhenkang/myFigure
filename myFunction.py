91e3d9e3447d5487d627feaa98d6f617863be3a3
qizhenkang <qizhenkang@sina.com>
2021-10-12 17:24:24 +0800

    new figure

--- myFunction.py
+++ myFunction.py
@@ -10,104 +10,17 @@ import calendar
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib import rcParams
+import qizhenkangpylib
 # 字体设置
 rcParams['font.family'] = 'Times New Roman'
 
-class myFunction:
-
-    def rename(self,file):
-        ''' file: 文件路径    keyWord: 需要修改的文件中所包含的关键字 '''
-        # start =time.clock()
-        # os.chdir(file)
-        items = os.listdir(file)
-        print(os.getcwd())
-        for name in items:
-            # print(name)
-            if name[:4] == 'Solu':
-                if name == 'SolutionReName.py':
-                    continue
-                if name[-2:] == 'py' or name[-2:] == 'pp':
-                    print(name)
-                    new_name = name[:9] + '0' + name[9:]
-                    print(new_name)
-
-    
-    def Name2MarkdownList(self,fileFolder):
-        items = os.listdir(fileFolder)
-        strlist = []
-        pointer = 0
-        for name in items:
-            if name[:9] == 'Solution_':
-                
-                
-                
-                # print(name)
-                strlist.append('')
-                strlist[pointer] += '| '
-                # Number
-                zeroFlag = 0
-                numstr = ''
-                for n in name[9:13]:
-                    if n != '0':
-                        zeroFlag = 1
-                    if zeroFlag:
-                        numstr += n
-                strlist[pointer] += numstr
-                strlist[pointer] += ' | '
-                # Title
-                titleStr = ''
-                for n in name[14:]:
-                    if n =='.':
-                        break
-                    titleStr += n
-                strlist[pointer] += titleStr
-                strlist[pointer] += ' | '
-                strlist[pointer] += ' | '
-                # Status
-                strlist[pointer] += '[AC](./' + name +')'
-                strlist[pointer] += ' | '
-                
-                # Time
-                # 获取文件时间
-                unixTime = os.path.getmtime(fileFolder+name)
-                localTime = time.localtime(unixTime)
-                timeStr = time.strftime('%Y/%m/%d',localTime)
-                strlist[pointer] += timeStr
-                strlist[pointer] += ' | '
-                strlist[pointer] += ' | '
-                strlist[pointer] += ' | '
-                
-                # print(strlist[pointer])
-                pointer +=1
-        return strlist
-    
-    def statistics(self,fileFolder):
-        """
-        我想统计一下我的做题时间、做题曲线
-        """
-        items = os.listdir(fileFolder)
-        # 每日提交时间统计
-        hourslist = [0] * 24
-        # 年份/月份统计 （当前共涉及2年）
-        yearsDict = {}
-        yearsDict[2020] = [0]*12
-        yearsDict[2021] = [0] * 12
-
-        for name in items:
-            if name[:9] == 'Solution_':
-                # 获取文件时间
-                unixTime = os.path.getmtime(name)
-                localTime = time.localtime(unixTime)
-                hourslist[localTime.tm_hour] +=1
-                yearsDict[localTime.tm_year][localTime.tm_mon-1] += 1
-
-        return hourslist,yearsDict
-                
-
 if __name__ == '__main__':
-    myfunc = myFunction()
+    # myfunc = myFunction()
     # Statistics
-    hourslist, yearsDict = myfunc.statistics('.')
+    # hourslist, yearsDict = myfunc.statistics('./code/')
+    hourslist = [4, 0, 0, 0, 0, 0, 0, 0, 0, 3, 22, 10, 5, 8, 10, 11, 7, 16, 1, 13, 16, 4, 2, 4]
+    yearsDict = {2020: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18], 2021: [0, 0, 0, 0, 0, 0, 0, 12, 68, 38, 0, 0]}
+    
     totalProblems = sum(hourslist)
     
     plt.style.use('dark_background')
@@ -131,7 +44,7 @@ if __name__ == '__main__':
     plt.xlabel('Time / hour',fontsize=fontSize+3)
     plt.ylabel('The percentage of problems / %',fontsize=fontSize+3)
     plt.title("Hourly Distribution of Zhenkang's Study Time", fontsize=fontSize+5) # 设置标题
-    plt.savefig('.//image//HourlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
+    plt.savefig('.//figure//HourlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
     
     # plt.xticks(fontsize = 30)
     
@@ -155,7 +68,7 @@ if __name__ == '__main__':
     plt.xlabel('Time / month',fontsize=fontSize+3)
     plt.ylabel('The percentage of problems / %',fontsize=fontSize+3)
     plt.title("Monthly Distribution of Zhenkang's Study Time", fontsize=fontSize+5) # 设置标题
-    plt.savefig('.//image//MonthlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
+    plt.savefig('.//figure//MonthlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
     # plt.xticks(fontsize = 30)
     
     
