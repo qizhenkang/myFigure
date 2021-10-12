8c8bdd1fa1892ddbdf41f35eca66cf7959b05064
qizhenkang <qizhenkang@sina.com>
2021-10-12 19:06:35 +0800

    Update figureGenerator.py

--- figureGenerator.py
+++ figureGenerator.py
@@ -18,20 +18,33 @@ if __name__ == '__main__':
     # myfunc = myFunction()
     # Statistics
     # hourslist, yearsDict = myfunc.statistics('./code/')
+    plt.close('all')
+    # hours' and years' DATA
     hourslist = [4, 0, 0, 0, 0, 0, 0, 0, 0, 3, 22, 10, 5, 8, 10, 11, 7, 16, 1, 13, 16, 4, 2, 4]
     yearsDict = {2020: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18], 2021: [0, 0, 0, 0, 0, 0, 0, 12, 68, 38, 0, 0]}
     
     totalProblems = sum(hourslist)
     
-    plt.style.use('dark_background')
+    plt.style.use('dark_background') # 设置Dark模式
     githubColor = [13/255,17/255,23/255] # 匹配github色彩
+    
+    #  --------------------------------------------------------
+    #   Bar Figure
+    #  --------------------------------------------------------
+    
+    # 获取对象
     fig_hours, ax = plt.subplots(figsize=(8, 4),facecolor=githubColor) # ,facecolor='black'
+    # x y 数据
     hours = [str(i) for i in range(24)]
     hourslist_percent = [ i / totalProblems * 100  for i in hourslist]
+    # 设置bar颜色，获取cmap映射
     hourscmap = plt.cm.get_cmap('tab20c')
     hoursColor = hourscmap([ i/24 * 4 / 5 for i in range(24)])
+    # 绘图
     ax.bar(hours, hourslist_percent, width=0.5, label="Working Hours", color = hoursColor)
+    # 设置底色
     ax.set_facecolor(githubColor)
+    # 设置x轴标签间隔显示
     for label in ax.xaxis.get_ticklabels()[1::2]:
         label.set_visible(False)
     
@@ -39,15 +52,15 @@ if __name__ == '__main__':
     # 设置字号
     fontSize = 10
     myDPI = 150
+    # 设置横纵坐标文字及字号
     plt.xticks(size=fontSize)
     plt.yticks(size=fontSize)
     plt.xlabel('Time / hour',fontsize=fontSize+3)
     plt.ylabel('The percentage of problems / %',fontsize=fontSize+3)
     plt.title("Hourly Distribution of Zhenkang's Study Time", fontsize=fontSize+5) # 设置标题
-    plt.savefig('.//figure//HourlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
-    
-    # plt.xticks(fontsize = 30)
-    
+    # 保存图像
+    # plt.savefig('.//figure//HourlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
+
     fig_months, ax = plt.subplots(figsize=(8, 4),facecolor=githubColor)
     months = [calendar.month_abbr[i] for i in range(1,13)]
     monthscmap = plt.cm.get_cmap('tab20c')
@@ -71,11 +84,45 @@ if __name__ == '__main__':
     plt.savefig('.//figure//MonthlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
     # plt.xticks(fontsize = 30)
     
+    #  --------------------------------------------------------
+    #   Line Figure
+    #  --------------------------------------------------------
     
-    # Name2MarkdownList
-    # strlist = myfunc.Name2MarkdownList('E:\\Python_Projects\\LeetCode\\')
-    # for s in strlist:
-    #     print(s)
+    # 获取对象
+    fig_Line, ax_Line = plt.subplots(figsize=(8, 4),facecolor=githubColor) # ,facecolor='black'
+    # x y 数据
+    totalProblems2021 = sum(yearsDict[2021])
+    months = [calendar.month_abbr[i] for i in range(1,13)]
+    monthslist = [i for i in yearsDict[2021]]
+    for i in range(1,len(monthslist)):
+        monthslist[i] += monthslist[i-1]
+    # 设置bar颜色，获取cmap映射
+    mycmap = plt.cm.get_cmap('tab20c')
+    myColor = mycmap([ i/12 * 4 / 5 for i in range(12)])
+    # 绘图
+    ax_Line.plot(months, monthslist)
+    # ax.plot(months, monthslist)
+    # 设置底色
+    ax_Line.set_facecolor(githubColor)
+    # 设置x轴标签间隔显示
+    # for label in ax_Line.xaxis.get_ticklabels()[1::2]:
+    #     label.set_visible(False)
     
-    # Rename
-    # myfunc.rename('E:\\Python_Projects\\LeetCode\\')
+    
+    # 设置字号
+    fontSize = 10
+    myDPI = 150
+    # 设置横纵坐标文字及字号
+    plt.xticks(size=fontSize)
+    plt.yticks(size=fontSize)
+    plt.xlabel('Time / hour',fontsize=fontSize+3)
+    plt.ylabel('The number of problems ',fontsize=fontSize+3)
+    plt.title("Total Solutions Curve", fontsize=fontSize+5) # 设置标题
+    # 保存图像
+    # plt.savefig('.//figure//HourlyDistribution.jpg', dpi=myDPI,facecolor=githubColor)
+
+    
+    
+    
+    
+
